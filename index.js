2f10a70ef13102a71c5a25613d6eab304eb96cee
Maksym <109230509+TheMaksym@users.noreply.github.com>
2023-10-14 23:32:12 -0400

    GPT functionality works-ish

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
-const OpenAI = require("openai");
-const { Configuration, OpenAIApi } = OpenAI;
-
-//React server
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const OpenAI = require("openai");
 const app = express();
 const port = 3001;
 
-const configuration = new Configuration({
-   organization: "org-YaAxZBMPiP7GafQ0Hnr4rbgq",
-   apikey: "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",
-});
-const openai = new OpenAIApi(configuration);
-//bodyParse & Cors
+const openai = new OpenAI({ apiKey:"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" });
+
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/', async (req, res) => {
-    // const messages = [{"role": "user", "content" : "test"}];
+app.post('/', async (req,res)=>{
+    const testingMessage = [{"role": "user", "content" : "this is a test"}];
     const response = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
-        prompt: "say this is a test",
+        messages: testingMessage,
         max_tokens: 7,
         temperature: 0,
     });
     console.log(response)
     res.json({
-        message: "hello world!" //response.data.choices[0].text;
+        message: "Hello World!"
     });
 });
 
-app.listen(port, () => {
-    console.log('Example app listening on port ' + port);
+app.listen(port, () =>{
+    console.log('example app listening at http://localhost:3000/')
 });
