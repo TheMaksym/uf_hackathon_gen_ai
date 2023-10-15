fcb213636a1acb7c0c48d74516467c971986b855
Maksym <109230509+TheMaksym@users.noreply.github.com>
2023-10-15 14:56:49 -0400

    REACT WORKS KINDA WELL

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 import React, { useState } from 'react';
 import './App.css';
+import './styles.css';
 
 function App() {
-  
-  const [message, setMessage] = useState('');
-  const [response, setResponse] = useState('');
+    const [message, setMessage] = useState('');
+    const [response, setResponse] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetch('http://localhost:3001/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ message }),
-    })
-        .then((res) => res.json())
-        .then((data) => setResponse(data.message))
-        .catch((error) => console.error('Error:', error));
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('http://localhost:3001/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ message }),
+        })
+            .then((res) => res.json())
+            .then((data) => setResponse(data.message))
+            .catch((error) => console.error('Error:', error));
+    };
 
-  return (
-      <div className="App">
-        <h1>Horizon ChatBot</h1>
-        <form onSubmit={handleSubmit}>
-          <textarea
+    return (
+        <div className="App">
+            <h1>Verizon ChatBot</h1>
+            <form onSubmit={handleSubmit}>
+        <textarea
+            className="input-box"
             value={message}
-            placeholder="Ask Horizon Anything"
-            onChange={(e)=> setMessage(e.target.value)}
-          ></textarea>
-          <button type="submit">Submit</button>
-        </form>
-        <div>{response}</div>
-      </div>
-  );
+            placeholder="Ask Verizon Anything"
+            onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+                <button className="button" type="submit">
+                    Submit
+                </button>
+            </form>
+            <div>{response}</div>
+        </div>
+    );
 }
 
-  export default App
+export default App;
